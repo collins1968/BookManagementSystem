a8dc5655c3cb61880c62cf0be5fcf09e8bbb43e4
Collins Mwendwa <collinsmwendwa1968@gmail.com>
2023-09-15 12:00:45 +0300

    Add project files.

---
+++ Models/AuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorFrontend.Models
+{
+    public class AuthorDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
---
+++ Models/BookDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFrontend.Models
+{
+    public class BookDto
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+
+        public string Description { get; set; } = string.Empty;
+
+
+        public List<AuthorDto> Authors { get; set; } = new List<AuthorDto>();
+
+    }
+}
